631d7f9e839e5b73fcbab4a5b8638820e96ffd88
Nick Inman <nick@theoatmeal.com>
2019-02-05 01:07:42 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -2,7 +2,7 @@ class StudentController < ApplicationController
 
   def students
     Student.all.each do |student|
-      @students = student.first_name + " " + student.last_name
+      @students << student.first_name + " " + student.last_name
     end
   end
 end
